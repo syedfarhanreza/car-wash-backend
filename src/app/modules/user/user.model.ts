f3e92d1507e6ef5bceddf74b8acc982662e26725
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2024-06-13 12:19:50 +0600

    user interface & model created

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,33 @@
+import { Schema, model } from "mongoose";
+import { TUser } from "./user.interface";
+
+const userSchema = new Schema<TUser>({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
+
+export const UserModel = model<TUser>("User", userSchema);
